96141a11d738b997c771dcb1fedc94c070a51296
Katerina Solstad (katya sorok) <katya.solstad@gmail.com>
2020-09-19 23:30:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+#comments
+#special string for multi-lined 
+comment_text='''comment
+       in many lines
+       as many lines as you need
+       but it is readable as text
+       you can use it if you want to
+       ask python to print text
+       in multiple line
+'''
+
+print(comment_text)
+
+# make a picture with letters and symbols
+#and print it as the example:
+
+print('''
+             *
+           *****
+        **********
+      ***************
+            ***
+            *** ''')
+
+'''we will use comments all the time in your homework
+   make sure you make your plan how to solve
+   a problem in comments. Example:'''
+
+# creating a variable to hold a picture
+my_pic='''
+            O    O
+            Y    Y
+            %%%%%%
+            %%**%%
+          <%%%  %%%>'''
+           
+#using python print() function to print the picture 
+
+print(my_pic)
